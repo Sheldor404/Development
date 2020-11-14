d3154b3946cf6c47246b8eefa62d042dfefec2fa
Nicolas <Nicolas@Nicolas>
2020-11-15 00:45:38 +0100

    update

---
+++ Usefull methods/src/mathfunctions/basics.java
@@ -0,0 +1,12 @@
+package mathfunctions;
+
+public class basics {
+	
+    public static double potentz(double x,int exponent) {
+    double y = x;
+    	for (int i = 0; i < exponent-1; i++) {
+    		y = y*x;
+    		}
+    	return y;
+    }
+}
