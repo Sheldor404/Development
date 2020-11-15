77b0314f9e53195813526b0c6157b9aff2e64f14
Nicolas <Nicolas@Nicolas>
2020-11-15 15:10:55 +0100

    wada

--- Usefull methods/src/mathfunctions/basics.java
+++ Usefull methods/src/mathfunctions/basics.java
@@ -2,11 +2,14 @@ package mathfunctions;
 
 public class basics {
 	
-    public static double potentz(double x,int exponent) {
+    public static double exponentiate(double x,int exponent) {
     double y = x;
     	for (int i = 0; i < exponent-1; i++) {
     		y = y*x;
     		}
     	return y;
     }
+    
+    
+    
 }
