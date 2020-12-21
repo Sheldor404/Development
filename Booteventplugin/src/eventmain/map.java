1cc9c95b232fdc043482bb2473f30a9d143fa645
Nicolas <gruternicolas@gmailcom>
2020-12-21 22:49:57 +0100

    awdawd

--- Booteventplugin/src/eventmain/map.java
+++ Booteventplugin/src/eventmain/map.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import org.bukkit.entity.Player;
 
 public class map {
-//testeifwahofs
+//testeifwahofsd
 	public map() {
 		 HashMap<Player, ArrayList<String>> map = new HashMap<>();
 		System.out.println("Test Test by Kyle");
